ce95e6960784e8b7f4528956252943c7bcd616a4
abisogno03 <a.bisogno53@studenti.unisa.it>
2025-01-18 17:49:23 +0100

    pagamento modifica

--- SushistarNgApp/src/app/app.routes.ts
+++ SushistarNgApp/src/app/app.routes.ts
@@ -6,12 +6,15 @@ import {ProductPageComponent} from '../components/product-page/product-page.comp
 import {ProfilePageComponent} from '../components/profile-page/profile-page.component';
 import {AuthGuard} from '../guards/auth.guards';
 import {CartComponent} from '../components/cart/cart.component';
-
+import {PaymentComponent} from '../components/payment/payment.component';
+import {SuccessComponent} from '../components/success/success.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'menu', component: MenuComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'payment', component: PaymentComponent },
+  {path: 'success', component: SuccessComponent },
   {path: 'user', component: ProfilePageComponent, canActivate: [AuthGuard]},
   {path: 'product/:name', component: ProductPageComponent},
   {path: 'cart', component: CartComponent, canActivate: [AuthGuard],},
